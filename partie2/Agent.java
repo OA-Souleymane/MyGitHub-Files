a971602313b4223b9ccf4b34d73cd730535b8555
oa-souleymane <oasouleycouly@gmail.com>
2021-05-31 23:23:08 +0100

    partie2 améliorée

--- partie2/Agent.java
+++ partie2/Agent.java
@@ -1,35 +1,58 @@
 package partie2;
 
-public class Agent {
+import com.rabbitmq.client.DeliverCallback;
 
-	public String affichageMsg(String message) {
-		int code = Integer.parseInt(message);           
-        
-        //construction des modèle de message
-        String type = "";
-		if(code == 0)
-		  type = "Cap";
-		else if(code == 1)
-			   type = "Spc";
-		else if(code == 2)
-			   type = "Rec";
-		else if(code == 3)
-			   type = "Res";
-		else
-		  type = "Int";
-		return "["+type+" msg : code message "+code+"]";
-	}
+public class Agent {
+	
+	static String message = "0";
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		
+//Publication des Capability------------------------------------------------------------------------------------------------------------
+		//agent first publish
+		PublishCapability agentP1 = new PublishCapability();
+		MessageBody msg1 = new MessageBody(0);
+		agentP1.Publish("CAPAILITY", msg1.toString());
+		Thread.sleep(3000);
 		
-	int code_msg = 0;
-		PublishCapability agent = new PublishCapability();
+//Traitement de la Specification ==> de la demande---------------------------------------------------------------
+		//agent first Receive
+		SubscribeOnCapability agentR1 = new SubscribeOnCapability();
+		do {
+			agentR1.Subscribe("SPEC");
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+        	 message = new String(delivery.getBody(), "UTF-8");
+        	 int code = Integer.parseInt(message);           
+               boolean spec = false;
+               //construction des modèle de message
+               String type = "";
+       		if(code == 0)
+       		  type = "Cap";
+       		else if(code == 1) {
+       			   type = "Spc";
+       			   spec = true;
+       		}else if(code == 2)
+       			   type = "Rec";
+       		else if(code == 3)
+       			   type = "Res";
+       		else
+       		  type = "Int";
+       		System.out.println("["+type+" msg : code message "+code+"]");
+        	};
+        	agentR1.canal.basicConsume("SPEC" , true, deliverCallback, consumerTag -> { }); 
+		} while(Integer.parseInt(message.toString()) != 1);       
+      	      
+      	      
+//Envoie des Resultats------------------------------------------------------------------------------------------
+		
+				//agent second publish
+	int code_msg = 3;
+		PublishCapability agentP2 = new PublishCapability();
 		//System.out.println(msg.toString());
 		while(true) {
-		MessageBody msg = new MessageBody(code_msg);
-		agent.Publish("QUEUE", msg.toString());
-		code_msg ++;
+		MessageBody msg2 = new MessageBody(code_msg);
+		agentP2.Publish("RES", msg2.toString());
+		//code_msg ++;
 		Thread.sleep(3000);
 		}
 	}
--- partie2/Controller.java
+++ partie2/Controller.java
@@ -1,33 +1,59 @@
 package partie2;
 
-public class Controller {
+import java.util.*;
 
-	public String affichageMsg(String message) {
-		int code = Integer.parseInt(message);           
-        
-        //construction des modèle de message
-        String type = "";
-		if(code == 0)
-		  type = "Cap";
-		else if(code == 1)
-			   type = "Spc";
-		else if(code == 2)
-			   type = "Rec";
-		else if(code == 3)
-			   type = "Res";
-		else
-		  type = "Int";
-		return "["+type+" msg : code message "+code+"]";		
-	}
-	
+import com.rabbitmq.client.DeliverCallback;
+
+public class Controller {
+	static String message = "";
 	public static void main(String[] args) throws Exception {
 		
-          SubscribeOnCapability controller = new SubscribeOnCapability();
-          controller.Subscribe("QUEUE");
-          while(true) {
-          controller.getMessage();
-          Thread.sleep(3000);
-          }
+		SubscribeOnCapability controllerR1 = new SubscribeOnCapability();
+		System.out.println("En attente de Spec msg ");
+		do {
+			System.out.print("!");
+			controllerR1.Subscribe("CAPAILITY");
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+        	 message = new String(delivery.getBody(), "UTF-8");
+        	 if(message != "")
+        		 System.out.println();
+        	 System.out.println(" message "+message);
+        	};
+		    Thread.sleep(1000);
+        	controllerR1.canal.basicConsume("CAPAILITY" , true, deliverCallback, consumerTag -> { }); 
+		} while(message == "");
+		
+		System.out.println("-----------------------------------------------");
+		int code_msg;
+		System.out.println("donnez une spécification");
+	    Scanner sc = new Scanner(System.in);
+	    code_msg = sc.nextInt();
+	    while(code_msg != 0 && code_msg != 1 && code_msg != 2 && code_msg != 3){
+			System.out.println("donnez une nouvelle spécification");
+		    code_msg = sc.nextInt();
+		}
+		PublishCapability controllerP1 = new PublishCapability();
+		MessageBody msg1 = new MessageBody(code_msg);
+		controllerP1.Publish("SPEC", msg1.toString());
+		Thread.sleep(1000);
+	
+		System.out.println("----------------------------------------------");
+		message = "";
+		SubscribeOnCapability controllerR2 = new SubscribeOnCapability();
+		do {
+        controllerR2.Subscribe("RES");
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+        	 message = new String(delivery.getBody(), "UTF-8");
+        	 System.out.println(" message "+message);
+        	};
+        	
+		
+		
+	        controllerR2.canal.basicConsume("RES" , true, deliverCallback, consumerTag -> { }); 
+		} while(message != "");
+		
+		System.out.println("moi");
+
 	}
 
 }
