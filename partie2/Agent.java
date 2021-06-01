da3b6b37215f0223d669b7b76e4ada57f3f1eaed
oa-souleymane <oasouleycouly@gmail.com>
2021-06-02 00:49:05 +0100

    partie2 avancement 3/4

--- partie2/Agent.java
+++ partie2/Agent.java
@@ -3,7 +3,7 @@ package partie2;
 import com.rabbitmq.client.DeliverCallback;
 
 public class Agent {
-	
+	static String inter_msg = "";
 	static String message = "0";
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
@@ -15,30 +15,36 @@ public class Agent {
 		PublishCapability agentP1 = new PublishCapability();
 		MessageBody msg1 = new MessageBody(0);
 		agentP1.Publish("CAPAILITY", msg1.toString());
-		
+		System.out.println("capability msg envoyé!");
+		System.out.println("-----------------------------------------");
 //-----------------------------------------------------------------------------------------------------------
 		//retrieve specification code
 		//agent first Receive
 		SubscribeOnCapability agentR1 = new SubscribeOnCapability();
+		System.out.println("Attente de spécification msg!");
 		do {
 			agentR1.Subscribe("SPEC");
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+			//System.out.print("!");
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
         	 message = new String(delivery.getBody(), "UTF-8");
-        	 int code = Integer.parseInt(message);           
+        	   int code = Integer.parseInt(message);           
                //construction des modèle de message
                String type = "";
-       		if(code == 0)
-       		  type = "Capability";
-       		else if(code == 1) 
-       			   type = "Spcification";
-       		else if(code == 2)
+       		   if(code == 0)
+       		     type = "Capability";
+       		   else if(code == 1) 
+       			  type = "Spcification";
+       		   else if(code == 2)
        			   type = "Receipt";
-       		else if(code == 3)
+       		   else if(code == 3)
        			   type = "Result";
-       		else
-       		  type = "Interruption";
-       		System.out.println("["+type+" msg : code message "+code+"]");
+       		   else
+       		      type = "Interruption";
+       		   System.out.println("["+type+" msg : code message "+code+"]");
+       		  
+       		  
         	};
+        	Thread.sleep(2000);
         	agentR1.canal.basicConsume("SPEC" , true, deliverCallback, consumerTag -> { }); 
 		} while(Integer.parseInt(message.toString()) != 1);       
       	      
@@ -47,14 +53,33 @@ public class Agent {
 	//publication des resultats avec le code == 1
 	//agent second publish
 	int code_msg = 3;
+	int accuse = 1;
+	while(true) {
 		PublishCapability agentP2 = new PublishCapability();
 		//System.out.println(msg.toString());
-		while(true) {
-		MessageBody msg2 = new MessageBody(code_msg);
+		MessageBody msg2;
+		if(accuse == 1) { //code 2==> receipt msg
+			 msg2 = new MessageBody(2);
+			 accuse ++;
+		}else //code 3==>result msg
+		     msg2 = new MessageBody(code_msg);	
+		
 		agentP2.Publish("RES", msg2.toString());
 		//code_msg ++;
-		Thread.sleep(3000);
-		}
+		Thread.sleep(1000);
+		
+		/*SubscribeOnCapability agentR2 = new SubscribeOnCapability();
+		agentR2.Subscribe("INTERRUPT");
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+        	inter_msg = new String(delivery.getBody(), "UTF-8");
+        	 
+        	 
+        };
+        agentR2.canal.basicConsume("INTERRUPT" , true, deliverCallback, consumerTag -> { });
+    	//agentR2.canal.close();
+    	if(inter_msg != "")
+   		 System.exit(1);*/
+	}
 	}
 
 }
--- partie2/Controller.java
+++ partie2/Controller.java
@@ -5,22 +5,24 @@ import java.util.*;
 import com.rabbitmq.client.DeliverCallback;
 
 public class Controller {
+	static String inter = "";
 	static String message = "";
 	public static void main(String[] args) throws Exception {
 	//-----------------------------------------------------------------------
 		//Capability retrieved
 		SubscribeOnCapability controllerR1 = new SubscribeOnCapability();
-		System.out.println("En attente de Spec msg ");
+		System.out.println("En attente de Capability msg ");
 		do {
 			System.out.print("!");
 			controllerR1.Subscribe("CAPAILITY");
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
         	 message = new String(delivery.getBody(), "UTF-8");
-        	 if(message != "")
+        	 if(message != "") {
         		 System.out.println();
-        	 System.out.println(" message "+message);
+        	     System.out.println(" message "+message);
+        	 }
         	};
-		    Thread.sleep(1000);
+		    Thread.sleep(2000);
         	controllerR1.canal.basicConsume("CAPAILITY" , true, deliverCallback, consumerTag -> { }); 
 		} while(message == "");
 //------------------------------------------------------------------------------------
@@ -42,20 +44,52 @@ public class Controller {
 //--------------------------------------------------------------------------------
 		//Monitoring the result
 		System.out.println("----------------------------------------------");
+		System.out.println("Result msg");
 		message = "";
 		SubscribeOnCapability controllerR2 = new SubscribeOnCapability();
+		int etat = 0;
 		do {
         controllerR2.Subscribe("RES");
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
         	 message = new String(delivery.getBody(), "UTF-8");
-        	 System.out.println(" message "+message);
+        	 int code = Integer.parseInt(message);           
+             //construction des modèle de message
+             String type = "";
+     		   if(code == 0)
+     		     type = "Capability";
+     		   else if(code == 1) 
+     			  type = "Spcification";
+     		   else if(code == 2)
+     			   type = "Receipt";
+     		   else if(code == 3)
+     			   type = "Result";
+     		   else
+     		      type = "Interruption";
+     		   System.out.println("["+type+" msg : code message "+code+"]");
         	};
-        	
-		
-		
 	        controllerR2.canal.basicConsume("RES" , true, deliverCallback, consumerTag -> { }); 
+		    /*if(inter != "") {
+		    	PublishCapability interrupt = new PublishCapability();
+				MessageBody inter_msg = new MessageBody(-1);
+				controllerP1.Publish("INTERRUPT", inter_msg.toString());
+				inter = "";*/
+		   
 		} while(message != "");
-
+		
+//----------------------------------------------------------------------------------------------------------------
+		//pour interrompre le l'envoie du resultat
+		/*System.out.println("-----------------------------------------------");
+		String interrupt_code = "";
+		System.out.println("voulez-vous interrompre O/N?");
+		interrupt_code = sc.nextLine();
+	    while(interrupt_code != ""){
+			System.out.println("Encore, voulez-vous interrompre O/N?");
+			interrupt_code = sc.nextLine();
+		}
+		PublishCapability interrupt = new PublishCapability();
+		MessageBody inter = new MessageBody(-1);
+		controllerP1.Publish("INTERRUPT", inter.toString());
+		Thread.sleep(1000);*/
 	}
 
 }
